7d361a5969e41cc2237f67dfa996da7248de74a5
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-04 12:29:13 +0300

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Marios Kanellopoulos on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
