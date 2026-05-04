b590d101523177d75433018b38fcea5ac87985c0
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-04 13:28:15 +0300

    updating homeView

--- BlossomMovie/Constants.swift
+++ BlossomMovie/Constants.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Constants {
+    static let homeString = "Home"
+    static let upcomingString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Download"
+    static let playString = "Play"
+    static let trendingMoviesString = "Trending Movies"
+    static let trendingTVString = "Trending TV"
+    static let topRatedMoviesString = "Top Rated Movies"
+    static let topRatedTVString = "Top Rated TV"
+    
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line"
+    
+    static let testTitleURL = "https://image.tmdb.org/t/p/w500/nnl6OWkyPpuMm595hmAxNW3rZFn.jpg"
+    static let testTitleURL2 = "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+    static let testTitleURL3 = "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+      
+}
+
+extension Text {
+    func ghostButton() -> some View {
+        self
+            .frame(width: 100, height: 50)
+            .foregroundStyle(.buttonText)
+            .bold()
+            .background {
+                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .stroke(.buttonBorder, lineWidth: 5)
+            }
+    }
+}
--- BlossomMovie/HomeView.swift
+++ BlossomMovie/HomeView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct HomeView: View {
+    var heroTestTitle = Constants.testTitleURL
+     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ScrollView {
+                LazyVStack {
+                    AsyncImage(url: URL(string: heroTestTitle)) {
+                        image in image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height * 0.85) 
+                    
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            Text(Constants.playString)
+                                .ghostButton()
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Text(Constants.downloadString)
+                                .ghostButton()
+                        }
+                    }
+                    
+                    HorizontalListView(header: Constants.trendingMoviesString)
+                    HorizontalListView(header: Constants.trendingTVString)
+                    HorizontalListView(header: Constants.topRatedMoviesString)
+                    HorizontalListView(header: Constants.topRatedTVString)
+                }
+            }
+        }
     }
 }
 
