1ff8bb820d39494548723e4ea9189183dcc8b2a4
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-04 13:36:45 +0300

    homeview completed

--- BlossomMovie/HomeView.swift
+++ BlossomMovie/HomeView.swift
@@ -18,6 +18,12 @@ struct HomeView: View {
                         image in image
                             .resizable()
                             .scaledToFit()
+                            .overlay {
+                                LinearGradient(
+                                    stops: [Gradient.Stop(color: .clear, location: 0.8),   Gradient.Stop(color: .gradient, location: 1)],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                            }
                     } placeholder: {
                         ProgressView()
                     }
