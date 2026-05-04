79be3a928a702b758bca58d1d75762bc61ee2659
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-04 13:01:20 +0300

    added images and update icons with constants

---
+++ BlossomMovie/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BlossomMovie
+//
+//  Created by Marios Kanellopoulos on 4/5/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
