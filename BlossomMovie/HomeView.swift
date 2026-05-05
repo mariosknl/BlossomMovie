6437a8c6c15594bf3d696070ddc518bd525d6bed
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-05 09:50:15 +0300

    multiple calls in parallel with async let

--- BlossomMovie/HomeView.swift
+++ BlossomMovie/HomeView.swift
@@ -53,9 +53,9 @@ struct HomeView: View {
                         }
                         
                         HorizontalListView(header: Constants.trendingMoviesString, titles: viewModel.trendingMovies)
-    //                    HorizontalListView(header: Constants.trendingTVString)
-    //                    HorizontalListView(header: Constants.topRatedMoviesString)
-    //                    HorizontalListView(header: Constants.topRatedTVString)
+                        HorizontalListView(header: Constants.trendingTVString, titles: viewModel.trendingTV)
+                        HorizontalListView(header: Constants.topRatedMoviesString, titles: viewModel.topRatedMovies)
+                        HorizontalListView(header: Constants.topRatedTVString, titles: viewModel.topRatedTV)
                     }
                 case .failed(let error):
                     Text("Error: \(error.localizedDescription)")
