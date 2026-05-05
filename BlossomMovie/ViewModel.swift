0178ffd386062d2c5bab08f1bd8db6e54ed13ecb
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-05 09:42:37 +0300

    helper function to buildURL created and ViewModel updated

--- BlossomMovie/ViewModel.swift
+++ BlossomMovie/ViewModel.swift
@@ -23,7 +23,7 @@ class ViewModel {
         homeStatus = .fetching
         
         do {
-            trendingMovies = try await dataFetcher.fetchTitles(for: "movie")
+            trendingMovies = try await dataFetcher.fetchTitles(for: "movie", by: "trending")
             homeStatus = .success
         } catch {
             print(error)
