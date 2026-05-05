fe2b7cf21722f052422da3a98f7e31162fd544c6
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-05 11:23:56 +0300

    navigation part 2

--- BlossomMovie/HomeView.swift
+++ BlossomMovie/HomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct HomeView: View {
     let viewModel = ViewModel()
     @State private var titleDetailPath = NavigationPath()
-     
+    
     var body: some View {
         NavigationStack(path: $titleDetailPath) {
             GeometryReader { geo in
@@ -54,21 +54,29 @@ struct HomeView: View {
                                 }
                             }
                             
-                            HorizontalListView(header: Constants.trendingMoviesString, titles: viewModel.trendingMovies)
-                            HorizontalListView(header: Constants.trendingTVString, titles: viewModel.trendingTV)
-                            HorizontalListView(header: Constants.topRatedMoviesString, titles: viewModel.topRatedMovies)
-                            HorizontalListView(header: Constants.topRatedTVString, titles: viewModel.topRatedTV)
+                            HorizontalListView(header: Constants.trendingMoviesString, titles: viewModel.trendingMovies) { title in
+                                titleDetailPath.append(title)
+                            }
+                            HorizontalListView(header: Constants.trendingTVString, titles: viewModel.trendingTV) { title in
+                                titleDetailPath.append(title)
+                            }
+                            HorizontalListView(header: Constants.topRatedMoviesString, titles: viewModel.topRatedMovies) { title in
+                                titleDetailPath.append(title)
+                            }
+                            HorizontalListView(header: Constants.topRatedTVString, titles: viewModel.topRatedTV) { title in
+                                titleDetailPath.append(title)
+                            }
                         }
-                        .navigationDestination(for: Title.self) { title in
-                            TitleDetailView(title: title)
-                        }
+                        
                     case .failed(let error):
                         Text("Error: \(error.localizedDescription)")
                     }
-                    
                 }
                 .task {
                     await viewModel.getTitles()
+                }
+                .navigationDestination(for: Title.self) { title in
+                    TitleDetailView(title: title)
                 }
             }
         }
